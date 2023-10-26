d9055f005d31a69945e3c842696cb3e4b89919fa
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-10-26 18:36:22 +0100

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:aimed_labs_assessment/home.dart';
 import 'package:aimed_labs_assessment/views/sign_up.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
